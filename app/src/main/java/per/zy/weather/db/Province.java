1da61b6d9548c7baf8425c7d5e1f563179c3edaa
zhangyu <x10_01x@sina.com>
2018-05-25 10:08:07 +0800

    城市选择列表完成

---
+++ app/src/main/java/per/zy/weather/db/Province.java
@@ -0,0 +1,39 @@
+package per.zy.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * @Author zy
+ * @Date 2018\5\23 0023
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
---
+++ app/src/main/java/per/zy/weather/util/HttpUtil.java
@@ -0,0 +1,21 @@
+package per.zy.weather.util;
+
+import android.util.Log;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * @Author zy
+ * @Date 2018\5\23 0023
+ */
+
+public class HttpUtil {
+
+    public static void sendOKHttpRequest(String address, okhttp3.Callback callback){
+        Log.i("----","----6666-----");
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
