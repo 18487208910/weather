d1ebede12b0bc62a27db9cee1ecfab055b509421
zhangyu <x10_01x@sina.com>
2018-05-26 15:35:20 +0800

    全屏显示，设置状态栏透明

--- app/src/main/java/per/zy/weather/WeatherActivity.java
+++ app/src/main/java/per/zy/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package per.zy.weather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -54,6 +56,13 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= 21){
+            View decoView = getWindow().getDecorView();
+            decoView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }else{}
         setContentView(R.layout.activity_weather);
 
         //初始化各控件
